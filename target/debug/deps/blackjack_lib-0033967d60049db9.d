e064d5ad82f87e74511f520d7c0adb84dcdab1fe
Benjamin Haase <benjaminthaase@protonmail.com>
2023-05-21 13:19:58 -0700

    Finished method that performs appropriate logic at the end of the turn, multiple methods needed to be added to both the table struct and the player struct. None of the prototypes have been tested in a live fire exercise, but it does compile

---
+++ target/debug/deps/blackjack_lib-0033967d60049db9.d
@@ -0,0 +1,5 @@
+/Users/benjaminhaase/development/Personal/rust_blackjack/target/debug/deps/blackjack_lib-0033967d60049db9: blackjack_lib/src/main.rs
+
+/Users/benjaminhaase/development/Personal/rust_blackjack/target/debug/deps/blackjack_lib-0033967d60049db9.d: blackjack_lib/src/main.rs
+
+blackjack_lib/src/main.rs:
