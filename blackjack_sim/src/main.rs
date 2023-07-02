d7e92b9ee57b4edfc768367223e65aa30d46ea4d
Benjamin Haase <benjaminthaase@protonmail.com>
2023-07-02 12:52:25 -0700

    Added counting strategies to binary crate

--- blackjack_sim/src/main.rs
+++ blackjack_sim/src/main.rs
@@ -1,6 +1,8 @@
 use blackjack_sim::strategy::{
-    BasicStrategy, CountingStrategy, HiLo, MarginBettingStrategy, PlayerStrategy, WongHalves, KO,
+    BasicStrategy, CountingStrategy, HiLo, HiOptI, HiOptII, MarginBettingStrategy, PlayerStrategy,
+    RedSeven, WongHalves, KO,
 };
+
 use blackjack_sim::{
     BlackjackSimulatorConfig, BlackjackSimulatorConfigBuilder, MulStrategyBlackjackSimulator,
     MulStrategyBlackjackSimulatorBuilder,
@@ -20,6 +22,21 @@ fn main() {
         ))
         .simulation(PlayerStrategy::new(
             KO::new(6),
+            BasicStrategy::new(),
+            MarginBettingStrategy::new(3.0, 5),
+        ))
+        .simulation(PlayerStrategy::new(
+            RedSeven::new(6),
+            BasicStrategy::new(),
+            MarginBettingStrategy::new(3.0, 5),
+        ))
+        .simulation(PlayerStrategy::new(
+            HiOptI::new(6),
+            BasicStrategy::new(),
+            MarginBettingStrategy::new(3.0, 5),
+        ))
+        .simulation(PlayerStrategy::new(
+            HiOptII::new(6),
             BasicStrategy::new(),
             MarginBettingStrategy::new(3.0, 5),
         ))
